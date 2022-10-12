2db34dabffa44cef86a9e905d7f90e6e9e2bdb9a
Olamide <olamide.olutekunbi@meltwater.org>
2022-10-12 21:21:13 +0000

    I just tested

---
+++ sum.py
@@ -0,0 +1,13 @@
+2#Addition of two numbers
+
+#creating the variable name for secondNumber
+firstNumber=input("Enter First Number: ")
+
+#creating the variable name for secondNumber
+secondNumber=input("Enter Second Number: ")
+
+#adding the value and passing to to the variable name sum
+sum=int(firstNumber)+int(secondNumber)
+
+#printing the sum value of the first and second number
+print(f"The sum of {firstNumber} and {secondNumber} is {sum}")
